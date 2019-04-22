d2ad85ec181ea6217428ff5d2901dc98100c1493
kinoodaphne <daphne.kinoo@outlook.be>
2019-04-22 11:42:57 +0200

    BULK/update Db.class.php
    Updatd Db.class.php so that you only have to use the config.ini file

--- classes/Db.class.php
+++ classes/Db.class.php
@@ -10,7 +10,7 @@
             } else {
                 $config = parse_ini_file("config/config.ini");
                 // no connection yet, create one!
-                self::$conn = new PDO('mysql:host=127.0.0.1;port='.$config['port'].';dbname='.$config['db_name'], $config['db_user'], $config['db_password']);
+                self::$conn = new PDO('mysql:host='.$config['host'].';port='.$config['port'].';dbname='.$config['db_name'], $config['db_user'], $config['db_password']);
                 return self::$conn;
             }
         }
