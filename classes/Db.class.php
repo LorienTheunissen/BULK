a3fc1659b4d89424ad5247e0a1f4645ae66036b6
Lorien <r0622279@student.thomasmore.be>
2019-04-23 22:18:45 +0200

    BULK / added PHP classes and bootstrap file

---
+++ classes/Db.class.php
@@ -0,0 +1,17 @@
+<?php
+
+    abstract class Db {
+        private static $conn;
+
+        public static function getInstance() {
+            if ( self::$conn != null ) {
+                // connection found! Return connection
+                return self::$conn;
+            } else {
+                $config = parse_ini_file("config/config.ini");
+                // no connection yet, create one!
+                self::$conn = new PDO('mysql:host='.$config['host'].';port='.$config['port'].';dbname='.$config['db_name'], $config['db_user'], $config['db_password']);
+                return self::$conn;
+            }
+        }
+    }
---
+++ classes/Security.class.php
@@ -0,0 +1,10 @@
+<?php 
+    class Security {
+        public static function hash($password) {
+            $options = [
+                'cost' => 15
+            ];
+            $hash = password_hash($password, PASSWORD_DEFAULT, $options);
+            return $hash;
+        }
+    }
