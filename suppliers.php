58a3411a9c4c41ee4fee88939924c6cbe80c88bc
Lorien <r0622279@student.thomasmore.be>
2019-04-27 13:16:46 +0200

    BULK / nav.inc.php / external file with nav code

--- suppliers.php
+++ suppliers.php
@@ -12,40 +12,7 @@
     <link href="css/balloon.css" rel="stylesheet">
 </head>
 <body>
-<nav>
-    <div class="logo">
-        <a href="#"><img alt="logo" src="images/logo/BULK_logo_white_slogan.svg"></a>
-    </div>
-    <div class="subnav">
-        <ul>
-            <li><a href="about.php">Over BULK</a></li>
-            <li><a href="contact.php">Contact</a></li>
-            <div class="dropdown">
-                <li><a href="#">Welkom, <span>Joris</span></a></li>
-                <div class="dropdown-content">
-                    <li><a href="#">Accountoverzicht</a></li>
-                    <li><a href="#">Mijn bestellingen</a></li>
-                    <li><a href="logout.php">Uitloggen</a></li>
-                </div>
-            </div>
-            <li data-balloon="Winkelmandje" data-balloon-pos="down"><a href="#"><img alt="basket" class="icon" src="images/icons/BULK_icon_white_basket.png"></a></li>
-        </ul>
-    </div>
-    <div class="nav">
-        <ul>
-            <li><a href="#">Start</a></li>
-            <li><a href="products.php">Producten</a></li>
-            <li><a href="suppliers.php">Leveranciers</a></li>
-            <li><a href="initiatives.php">Initiatieven</a></li>
-        </ul>
-    </div>
-    <div class="search">
-        <form action="">
-            <input type="text" placeholder="Wat zoek je?" class="search">
-            <a href="#" class="search"><img alt="search" class="icon" src="images/icons/BULK_icon_white_search.png"></a>
-        </form>
-    </div>
-</nav>
+<?php include_once("includes/nav.inc.php"); ?>
 <div class="container">
     <div class="main">
         <div class="title">
