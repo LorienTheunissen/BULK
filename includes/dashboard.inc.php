448605e097403086610d88245abb5b5611663872
Lorien <r0622279@student.thomasmore.be>
2019-07-02 19:45:32 +0200

    Last update before summer starts!

---
+++ includes/dashboard.inc.php
@@ -0,0 +1,16 @@
+<aside>
+    <div class="logo">
+        <h1><a href="#">BULK.</a></h1>
+    </div>
+    <nav>
+        <a href="dashboard.php"><span><i class="fas fa-home"></i></span>Startpagina</a>
+        <a href="addproduct.php"><i class="fas fa-plus"></i>Product toevoegen</a>
+        <a href="myproducts.php"><i class="fas fa-dolly"></i>Producten</a>
+        <a href="#"><i class="fas fa-envelope"></i>Berichten</a>
+        <a href="#"><i class="fas fa-star"></i>Reviews</a>
+        <a href="index.php"><i class="fas fa-sign-out-alt"></i>Uitloggen</a>
+    </nav>
+</aside>
+<header>
+    <h2>Think <span style="color: #306a53">bigger</span>, Think <span style="color: #306a53">better</span>, Think <span style="color: #306a53">BULK.</span></h2>
+</header>
---
+++ includes/header.inc.php
@@ -0,0 +1,26 @@
+<div class="header">
+    <div class="header__logo">
+        <a href="#"><h1>BULK.</h1></a>
+    </div>
+    <div class="header__nav">
+        <ul>
+            <li><a href="start.php">Zoeken</a></li>
+            <li><a href="products.php">Producten</a></li>
+            <li><a href="suppliers.php">Leveranciers</a></li>
+        </ul>
+    </div>
+    <div class="header__subnav">
+        <ul>
+            <div class="dropdown">
+                <li><a href="#"><img src="images/icons/BULK_icon_white_user.png" alt="icon_profile" class="icon"></a></li>
+                <div class="dropdown-content">
+                    <li><a href="#">Accountoverzicht</a></li>
+                    <li><a href="index.php">Uitloggen</a></li>
+                </div>
+            </div>
+            <li><a href="start.php"><img src="images/icons/BULK_icon_white_search.png" alt="icon_search" class="icon"></a></li>
+            <!--<li><a href="#"><img src="images/icons/BULK_icon_white_message.png" alt="icon_search" class="icon"></a></li>-->
+            <li><a href="checkout.php"><img src="images/icons/BULK_icon_white_basket.png" alt="icon_basket" class="icon"></a></li>
+        </ul>
+    </div>
+</div>
